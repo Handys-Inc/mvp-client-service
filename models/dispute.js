2361a934806dc7fc604d5766e87f7b8039b7ba7d
Sylvia Monalisa Engmann <sylviaengmann@theMambaMentality-2.local>
2023-03-23 07:39:19 +0000

    create dispute route

---
+++ models/dispute.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+
+const disputeSchema = new mongoose.Schema({
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    serviceProvider: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'ServiceProvider'
+    },
+    bookingCode: {
+        type: String,
+        required: true,
+        ref: 'Booking'
+    },
+    disputeType: {
+        type: String,
+        enum: ['poorQuality', 'harassment', 'unprofessional']
+    },
+    information: {
+        type: String
+    },
+    disputeImages: [
+        {
+            type: String,
+            description: String
+        }
+    ],
+    resolvedFor: {
+        type: String,
+        enum: ['client', 'serviceProvider', 'none'],
+        default: 'none',
+    },
+    disputeStatus: {
+        type: String,
+        enum: ['resolved', 'unresolved'],
+        default: 'unresolved'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    
+});
+
+const Dispute = mongoose.model('Dispute', disputeSchema);
+
+module.exports.Dispute = Dispute;
--- routes/clients.js
+++ routes/clients.js
@@ -7,9 +7,19 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
+const { storage } = require("../config/cloudinary");
+
+const multer = require('multer');
+
+const upload = multer({
+    storage: storage
+})
+
 
 router.put("/booking/:id/cancel", auth, clientController.cancelBooking);
 router.get("/booking/:id/", auth, clientController.checkCancellation);
+router.get("/history", auth, clientController.getServiceHistory);
+router.post("/dispute", auth, upload.array('disputeImages'), clientController.createDispute);
 
 
 
