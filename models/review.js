346c373d4bc32b54b7f332729a7cbbe5440e77d2
Sylvia Engmann <sylvia.engmann8@gmail.com>
2023-04-27 17:56:01 +0000

    updating model

--- models/review.js
+++ models/review.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const reviewSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        required: true,
+        //required: true,
         ref: 'User'
     },
     serviceProvider: {
